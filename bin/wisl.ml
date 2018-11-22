1035c2815cc74800e4a44e645aa52c079a83da82
Sacha-Elie Ayoun <sacha-elie.ayoun@supelec.fr>
2018-11-22 13:56:16 +0000

    feat(binary test): server correctly sends notification on change

--- bin/wisl.ml
+++ bin/wisl.ml
@@ -1,8 +1,10 @@
+open Lsp.Scalars
+
 let timeofday = string_of_float (Unix.gettimeofday ())
 
 let logc = open_out ("test-" ^ timeofday ^ ".log")
 
-module Params : Lsp.P = struct
+module Params : Lsp.Server.P = struct
   let language_name = "test"
   let language_id = "test"
 
@@ -11,12 +13,32 @@ module Params : Lsp.P = struct
   let logc = logc
 
   module Hooks = struct
-    let onDidChangeContent _ =
-      Printf.fprintf logc "THIS IS A USER DFEFINED STUFFF HAHAHHAHAHHAHAHA"
+    let onDidChangeContent textDoc =
+      let Lsp.TextDocument.{text;uri;_} = textDoc in
+      let range = Range.{
+        rstart = Position.{
+          line = 0;
+          character = 0;
+        };
+        rend = Position.{
+          line = 0;
+          character = 3;
+        }
+      } in
+      let diag = Lsp.Diagnostic.{
+        range;
+        severity = Some Lsp.Diagnostic.Severity.SevError;
+        message = "AAAAAAAAAA";
+        code = None;
+        source = "wisl";
+        relatedInformation = [];
+      } in
+      Lsp.Actions.diagnose uri [diag]
+
   end
 end
 
-module Server : Lsp.S = Lsp.Make(Params)
+module Server = Lsp.Server.Make(Params)
 
 let main () =
   let r = Server.start () in
