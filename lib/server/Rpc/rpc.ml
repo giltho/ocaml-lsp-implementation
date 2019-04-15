df484911d8f3f8b94959ece4b69fe6905302f2f0
Sacha-Elie Ayoun <sachaayoun@gmail.com>
2019-04-15 16:05:12 +0100

    feat(rpc): parametrizable channels

--- lib/server/Rpc/rpc.ml
+++ lib/server/Rpc/rpc.ml
@@ -1,6 +1,5 @@
-let read_yojson () =
-  let input = Lwt_io.stdin in
-  let%lwt clength = Lwt_io.read_line input in
+let read_yojson ?(channel = Lwt_io.stdin) () =
+  let%lwt clength = Lwt_io.read_line channel in
   let cl = "Content-Length: " in
   let cll = String.length cl in
   if String.sub clength 0 cll = cl then (
@@ -13,7 +12,7 @@ let read_yojson () =
     in
     (* num is supposedly the length of the json message *)
     let buffer = Bytes.create num in
-    let%lwt () = Lwt_io.read_into_exactly input buffer 0 num in
+    let%lwt () = Lwt_io.read_into_exactly channel buffer 0 num in
     let raw = Bytes.to_string buffer in
     let json_or_error =
       try Ok (Json.parse raw) with
@@ -25,12 +24,11 @@ let read_yojson () =
     Lwt.return json_or_error)
   else Lwt.return (Error (Printf.sprintf "Invalid header"))
 
-let write_yojson outyj =
-  let output = Lwt_io.stdout in
+let write_yojson ?(channel = Lwt_io.stdout) outyj =
   let content = Json.json_to_string outyj in
   let sep = "\r\n\r\n" in
   let cl = String.length content in
   let cls = string_of_int cl in
   let header = Printf.sprintf "Content-Length: %s" cls in
   let message = header ^ sep ^ content in
-  Lwt_io.write output message
+  Lwt_io.write channel message
