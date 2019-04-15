140dac444b5d243eb63e5256c9e346d9dccdfd7a
Sacha-Elie Ayoun <sachaayoun@gmail.com>
2019-04-15 15:23:53 +0100

    refactor(rpc): rpc read/write using lwt

--- lib/server/Rpc/rpc.ml
+++ lib/server/Rpc/rpc.ml
@@ -1,6 +1,6 @@
 let read_yojson () =
-  let input = Channels.get_inc () in
-  let clength = input_line input in
+  let input = Lwt_io.stdin in
+  let%lwt clength = Lwt_io.read_line input in
   let cl = "Content-Length: " in
   let cll = String.length cl in
   if String.sub clength 0 cll = cl then (
@@ -12,29 +12,25 @@ let read_yojson () =
       int_of_string numstr + match Sys.os_type with "Win32" -> 1 | _ -> 2
     in
     (* num is supposedly the length of the json message *)
-    let buffer = Buffer.create num in
-    Buffer.add_channel buffer input num;
-    let raw = Buffer.contents buffer in
+    let buffer = Bytes.create num in
+    let%lwt () = Lwt_io.read_into_exactly input buffer 0 num in
+    let raw = Bytes.to_string buffer in
     let json_or_error =
-      try Ok (YojsonShort.parse raw) with
+      try Ok (Json.parse raw) with
       | Failure message ->
           Error
-            (ErrorCodes.ParseError
-               (Printf.sprintf "Cannot parse: %s as json. Message : %s" raw
-                  message))
-      | err ->
-          Error
-            (ErrorCodes.InternalError
-               (Printf.sprintf "Failure : %s" (Printexc.to_string err)))
+            (Printf.sprintf "Cannot parse: %s as json. Message : %s" raw
+                message)
     in
-    json_or_error )
-  else Error (ErrorCodes.ParseError (Printf.sprintf "Invalid header"))
+    Lwt.return json_or_error)
+  else Lwt.return (Error (Printf.sprintf "Invalid header"))
 
 let write_yojson outyj =
-  let content = YojsonShort.json_to_string outyj in
+  let output = Lwt_io.stdout in
+  let content = Json.json_to_string outyj in
   let sep = "\r\n\r\n" in
   let cl = String.length content in
   let cls = string_of_int cl in
   let header = Printf.sprintf "Content-Length: %s" cls in
   let message = header ^ sep ^ content in
-  Channels.out message
+  Lwt_io.write output message
--- lib/server/Rpc/rpc.mli
+++ lib/server/Rpc/rpc.mli
@@ -1,7 +1,7 @@
 (** Contains handler that take raw rpc messages and output something usable *)
 
-val read_yojson : unit -> Json.json result
+val read_yojson : unit -> ((Json.json, string) result) Lwt.t
 (** Reads a message and gives a yojson *)
 
-val write_yojson : Json.json -> unit
-(** Sens a yojson to the client *)
+val write_yojson : Json.json -> unit Lwt.t
+(** Sends a yojson to the client *)
