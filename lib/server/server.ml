0fbd9435de1a8ecc4ade2967bccbc3f96a251321
Sacha-Elie Ayoun <sachaayoun@gmail.com>
2019-04-16 16:08:14 +0100

    feat(server): server now use proper logging channels

--- lib/server/server.ml
+++ lib/server/server.ml
@@ -5,22 +5,6 @@ module type P = sig end
 module Make (P : P) = struct
   module MessageHandler = Handlers.MessageHandler.Make ()
 
-  let log_channel = Lwt_io.open_file ~mode:Lwt_io.Output "LSP_LOG_FILE.log"
-
-  let log s =
-    let%lwt channel = log_channel in
-    Lwt_io.write channel s
-
-  let log_json json = log (Json.json_to_string json)
-
-  let log_result res =
-    let json =
-      match res with
-      | Ok message -> Protocol.Message.to_yojson message
-      | Error ec -> Protocol.ErrorCodes.to_yojson ec
-    in
-    log_json json
-
   let rec loop () =
     let%lwt raw_json_res = Rpc.read_yojson () in
     let message_res =
@@ -28,7 +12,9 @@ module Make (P : P) = struct
       | Ok json -> Protocol.Message.of_yojson json
       | Error s -> Error (Protocol.ErrorCodes.ParseError s)
     in
-    let _ = log_result message_res in
+    let _ =
+      Actions.Log.log_result ~to_json:Protocol.Message.to_yojson message_res
+    in
     let _ =
       match message_res with
       | Ok m -> MessageHandler.handle m
@@ -36,7 +22,14 @@ module Make (P : P) = struct
     in
     loop ()
 
-  let run () = Lwt_main.run (loop ())
+  let start_server () =
+    let%lwt log_channel =
+      Lwt_io.open_file ~mode:Lwt_io.Output "LSP_LOG_FILE.log"
+    in
+    let () = State.Internal.set_log_channel log_channel in
+    loop ()
+
+  let run () = Lwt_main.run (start_server ())
 
   let start () = Term.(exit @@ eval (const run $ const (), info "Lsp"))
 end
